cf8d963d08dba9717a747c4a3114e57ba9f79de4
Farshid Ahsan <35967833+Farshid-997@users.noreply.github.com>
2021-09-26 22:38:31 +0600

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <div className="header">
 
             <h1>Favourite Band's Concert Planner </h1>
-
+<blockquote>Without music, life would be a mistake ― Friedrich Nietzsche, Twilight of the Idols.</blockquote>
             <h3> <strong> Total amount If You want to See Them:200 Million</strong></h3>
         </div>
     );
